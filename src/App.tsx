27a7b80b34de75b271ef6f5b358fa6c6d681252d
dev-jwshin <dev@foryourdev.com>
2025-07-28 14:52:23 +0900

    App.tsx 파일에서 컴포넌트 임포트 순서를 정리하고, 사용자 리소스 설정을 주석 처리하여 코드 가독성을 향상시켰습니다. 또한, 다국어 지원을 위한 로케일 설정을 개선하고, 관리자 사이드바의 스타일을 수정했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import {
-  Refine,
-  Authenticated,
-} from "@refinedev/core";
-import {
-  useNotificationProvider,
-  ThemedLayoutV2,
   ErrorComponent,
   RefineThemes,
   Sider,
+  ThemedLayoutV2,
+  useNotificationProvider,
 } from "@refinedev/antd";
-import {
-  UserOutlined,
-} from "@ant-design/icons";
+import { Authenticated, Refine } from "@refinedev/core";
 
+import routerProvider, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import { App as AntdApp, ConfigProvider } from "antd";
+import koKR from "antd/locale/ko_KR";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import { authProviderClient } from "./providers/auth-provider";
 import { dataProvider } from "./providers/data-provider";
 import { i18nProvider } from "./providers/i18n-provider";
-import routerProvider, {
-  NavigateToResource,
-  CatchAllNavigate,
-  UnsavedChangesNotifier,
-  DocumentTitleHandler,
-} from "@refinedev/react-router";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router";
-import { App as AntdApp, ConfigProvider } from "antd";
-import koKR from "antd/locale/ko_KR";
 
 // koKR를 상속받아서 일부만 커스터마이징
 const customLocale: Locale = {
@@ -32,22 +26,22 @@ const customLocale: Locale = {
   // Pagination 커스터마이징
   Pagination: {
     ...koKR.Pagination,
-    items_per_page: '개씩 보기',
+    items_per_page: "개씩 보기",
   },
   // Table 커스터마이징
   Table: {
     ...koKR.Table,
-    emptyText: '데이터가 없습니다',
+    emptyText: "데이터가 없습니다",
   },
 }; // TypeScript 타입 체크 우회
 
 import "@refinedev/antd/dist/reset.css";
 import "./App.css";
 
-import { UserList, UserEdit, UserShow, UserCreate } from "../src/pages/users";
-import { LoginPage } from "../src/pages/login";
-import { Header } from "./components/header";
 import { Locale } from "antd/es/locale";
+import { LoginPage } from "../src/pages/login";
+import { UserCreate, UserEdit, UserList, UserShow } from "../src/pages/users";
+import { Header } from "./components/header";
 
 const App: React.FC = () => {
   return (
@@ -59,18 +53,20 @@ const App: React.FC = () => {
             dataProvider={dataProvider}
             routerProvider={routerProvider}
             i18nProvider={i18nProvider}
-            resources={[
-              {
-                name: "users",
-                list: "/users",
-                show: "/users/show/:id",
-                edit: "/users/edit/:id",
-                create: "/users/create",
-                meta: {
-                  label: "사용자",
-                },
-              },
-            ]}
+            resources={
+              [
+                // {
+                //   name: "users",
+                //   list: "/users",
+                //   show: "/users/show/:id",
+                //   edit: "/users/edit/:id",
+                //   create: "/users/create",
+                //   meta: {
+                //     label: "사용자",
+                //   },
+                // },
+              ]
+            }
             notificationProvider={useNotificationProvider}
             options={{
               syncWithLocation: true,
@@ -84,7 +80,18 @@ const App: React.FC = () => {
                     key="authenticated-routes"
                     fallback={<CatchAllNavigate to="/login" />}
                   >
-                    <ThemedLayoutV2 Header={() => <div></div>} Sider={() => <Sider Title={() => <div className="text-white text-3xl text-center p-4">관리자</div>} />} >
+                    <ThemedLayoutV2
+                      Header={() => <div></div>}
+                      Sider={() => (
+                        <Sider
+                          Title={() => (
+                            <div className="text-white text-2xl text-center p-4">
+                              관리자
+                            </div>
+                          )}
+                        />
+                      )}
+                    >
                       <Outlet />
                     </ThemedLayoutV2>
                   </Authenticated>
@@ -107,21 +114,18 @@ const App: React.FC = () => {
                   </Authenticated>
                 }
               >
-                <Route
-                  path="/login"
-                  element={<LoginPage />}
-                />
+                <Route path="/login" element={<LoginPage />} />
               </Route>
 
-                              <Route
-                  element={
-                    <Authenticated key="catch-all">
-                      <ThemedLayoutV2 Header={() => <Header />}>
-                        <Outlet />
-                      </ThemedLayoutV2>
-                    </Authenticated>
-                  }
-                >
+              <Route
+                element={
+                  <Authenticated key="catch-all">
+                    <ThemedLayoutV2 Header={() => <Header />}>
+                      <Outlet />
+                    </ThemedLayoutV2>
+                  </Authenticated>
+                }
+              >
                 <Route path="*" element={<ErrorComponent />} />
               </Route>
             </Routes>
